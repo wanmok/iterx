76c67661774fffb59d4dd08ac9027df7e4297eda
Siddharth Vashishtha <svashis3@brtx601.idies.jhu.edu>
2023-11-07 18:47:19 -0500

    added role_prefix configs and renamed old ones

---
+++ src/jobs/train_famus_with_role_prefix/train_famus_source_gold_spans.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=brtx6
+#SBATCH --time=24:0:0
+#SBATCH --gpus=1
+
+eval "$(conda shell.bash hook)"
+conda activate iterx
+
+cd /brtx/601-nvme1/svashis3/iterx
+
+PYTHONPATH=./src allennlp train \
+  --include-package iterx \
+  -s models/roles_with_prefix/famus_model_source_data_gold_spans \
+  /brtx/601-nvme1/svashis3/iterx/resources/training_configs/famus_with_prefix/famus_source_config_gold_spans.jsonnet
